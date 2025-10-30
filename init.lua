50d2e726b3f50c0e9cbd870dfed424f3df1ebc35
BioBaby <pcc3cn03@gmail.com>
2025-10-30 23:59:37 +0900

    Add kanagawwa color scheme

--- init.lua
+++ init.lua
@@ -898,6 +898,21 @@ require('lazy').setup({
     end,
   },
 
+  -- {
+  --   'rebelot/kanagawa.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   config = function()
+  --     ---@diagnostic disable-next-line: missing-fields
+  --     require('kanagawa').setup {
+  --       styles = {
+  --         comments = { italic = false }, -- Disable italics in comments
+  --       },
+  --     }
+  --
+  --     vim.cmd.colorscheme 'kanagawa-wave'
+  --   end,
+  -- },
+
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
